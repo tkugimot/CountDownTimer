48eb1897b5d94665b73f01957cff1ad35e2f8af0
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2024-01-05 12:33:08 +0900

    Initial Commit

---
+++ CountDownTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDownTimer
+//
+//  Created by Toshimitsu Kugimoto on 2024/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDownTimer/CountDownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownTimerApp.swift
+//  CountDownTimer
+//
+//  Created by Toshimitsu Kugimoto on 2024/01/05.
+//
+
+import SwiftUI
+
+@main
+struct CountDownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
